0d0b66cc95cad07b5534dc488b56c736672b3bbe
Feroskhan Hasenkhan <49962087+fehase@users.noreply.github.com>
2019-09-13 17:03:09 -0700

    removed secrets

--- AAD-Security/Azure Resource Configuration/Configure_Azure_Security_Center.ps1
+++ AAD-Security/Azure Resource Configuration/Configure_Azure_Security_Center.ps1
@@ -1,16 +1,15 @@
-﻿Set-AzContext -Subscription "d07c0080-170c-4c24-861d-9c817742786c"
+﻿Set-AzContext -Subscription "$subscriptionid"
 Register-AzResourceProvider -ProviderNamespace 'Microsoft.Security'
 
-Set-AzContext -Subscription "d07c0080-170c-4c24-861d-9c817742786c"
 Set-AzSecurityPricing -Name "default" -PricingTier "Standard"
 Set-AzSecurityWorkspaceSetting -Name "default" -Scope
-"/subscriptions/d07c0080-170c-4c24-861d-9c817742786c" -WorkspaceId"/subscriptions/d07c0080-170c-4c24-861d-9c817742786c/resourceGroups/myRg/providers/Microsoft.OperationalInsights/workspaces/myWorkspace"
-Set-AzContext -Subscription "d07c0080-170c-4c24-861d-9c817742786c"
+"/subscriptions/$subscriptionid" -WorkspaceId"/subscriptions/$subscriptionid/resourceGroups/myRg/providers/Microsoft.OperationalInsights/workspaces/myWorkspace"
+Set-AzContext -Subscription "$subscriptionid"
 
 Set-AzSecurityAutoProvisioningSetting -Name "default" -EnableAutoProvision
 
-Set-AzSecurityContact -Name "default1" -Email "destsec@outlook.com" -Phone "3022646852" -AlertAdmin -NotifyOnAlert
+Set-AzSecurityContact -Name "default1" -Email "destsec@outlook.com" -Phone "4254248888" -AlertAdmin -NotifyOnAlert
 
 Register-AzResourceProvider -ProviderNamespace 'Microsoft.PolicyInsights'
 $Policy = Get-AzPolicySetDefinition | where {$_.Properties.displayName -EQ '[Preview]: Enable Monitoring in Azure Security Center'}
-New-AzPolicyAssignment -Name 'ASC Default <d07c0080-170c-4c24-861d-9c817742786c>' -DisplayName 'Security Center Default <subscription ID>' -PolicySetDefinition $Policy -Scope '/subscriptions/d07c0080-170c-4c24-861d-9c817742786c'
+New-AzPolicyAssignment -Name 'ASC Default <$subscriptionid>' -DisplayName 'Security Center Default <subscription ID>' -PolicySetDefinition $Policy -Scope '/subscriptions/$subscriptionid'
